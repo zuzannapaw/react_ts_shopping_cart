6fbd2635112d31583eaffbd114b9d044f99cccc1
ZuzannaPaw <zuzannapaw@wp.pl>
2023-08-16 15:54:09 +0200

    feat: shoppig-cart

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { Container, Navbar as NavbarBs } from "react-bootstrap";
+
+export const Navbar = () => {
+  return (
+    <NavbarBs className="bg-white shadow-sm mb-3">
+      <Container>Navbar</Container>
+    </NavbarBs>
+  );
+};
